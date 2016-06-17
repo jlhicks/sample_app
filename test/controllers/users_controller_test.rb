b1831cffe12b43bac8b30bdc1dc85eefc1d39762
James L. Hicks <jlh.zorch@gmail.com>
2016-06-17 23:19:36 +0000

    Add following and followers pages.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,17 +1,17 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  
+
   def setup
     @user = users(:michael)
     @other_user = users(:archer)
   end
-  
+
   test "should redirect index when not logged in" do
     get :index
     assert_redirected_to login_url
   end
-  
+
   test "should get new" do
     get :new
     assert_response :success
@@ -23,27 +23,27 @@ class UsersControllerTest < ActionController::TestCase
     assert_not flash.empty?
     assert_redirected_to login_url
   end
-  
+
   test "should redirect update when not logged in" do
     patch :update, id: @user, user: { name: @user.name, email: @user.email }
     assert_not flash.empty?
     assert_redirected_to login_url
   end
-  
+
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
     get :edit, id: @user
     assert flash.empty?
     assert_redirected_to root_url
   end
-  
+
   test "should redirect update when logged in as wrong user" do
     log_in_as(@other_user)
     patch :update, id: @user, user: { name: @user.name, email: @user.email }
     assert flash.empty?
     assert_redirected_to root_url
   end
-  
+
   test "should not allow the admin attribute to be edited via the web" do
     log_in_as(@other_user)
     assert_not @other_user.admin?
@@ -52,19 +52,29 @@ class UsersControllerTest < ActionController::TestCase
                                             admin: true }
     assert_not @other_user.reload.admin?
   end
-  
+
   test "should redirect destroy when not logged in" do
     assert_no_difference 'User.count' do
       delete :destroy, id: @user
     end
     assert_redirected_to login_url
   end
-  
+
   test "should redirect destroy when logged in as a non-admin" do
     log_in_as(@other_user)
     assert_no_difference 'User.count' do
       delete :destroy, id: @user
     end
     assert_redirected_to root_url
+  end
+
+  test "should redirect following when not logged in" do
+    get :following, id: @user
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get :followers, id: @user
+    assert_redirected_to login_url
   end
 end
