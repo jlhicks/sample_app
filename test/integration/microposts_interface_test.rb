9ec21af3f81afc580e60370b6232e72b3ee10073
James L. Hicks <jlh.zorch@gmail.com>
2016-06-17 23:00:54 +0000

    Add stats and a follow form.

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -40,7 +40,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   test "micropost sidebar count" do
     log_in_as(@user)
     get root_path
-    assert_match "#{@user.microposts.length} microposts", response.body
+    assert_match "#{@user.microposts.count} microposts", response.body
     # User with zero microposts
     other_user = users(:malory)
     log_in_as(other_user)
